f26935e5353b7c7a166f32885cc9fed66ad33e19
gaballench <gaballench@gmail.com>
2024-05-09 18:35:46 -0300

    Had a wrong type Float62

--- src/structs.jl
+++ src/structs.jl
@@ -3,7 +3,7 @@
 # data are being used by the loglik inside the Turing model
 #*_prior are passed on to the parameter priors inside the Turing model 
 struct StratInterval
-    data::Vector{Float62}
+    data::Vector{Float64}
     theta1_prior::ContinuousUnivariateDistribution
     theta2_prior::ContinuousUnivariateDistribution
     lambda_prior::ContinuousUnivariateDistribution
