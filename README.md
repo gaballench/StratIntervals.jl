50d3677fe1894b2a4563eba253daf8923566c6bf
gaballench <gaballench@gmail.com>
2024-05-09 17:44:30 -0300

    Set up documenter workflow, modified readme, and added the logo

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# StratIntervals
+# StratIntervals <img src="docs/src/logo.png" height="140" align="right"></img>
 
 [![Build Status](https://github.com/gaballench/StratIntervals.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/gaballench/StratIntervals.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+
+## Description
+
+A Julia package for Bayesian inference of stratigraphic intervals using time occurrence data.
+
+## Installation
+
+For now only the github development version is available. You can install using
+:
+
+```julia
+using Pkg
+Pkg.add("gaballench/StratIntervals.jl")
+```
+
+## Authorship
+
+This package was created and maintained by Gustavo A. Ballen
+(@gaballench).
+
+## Problems?
+
+If you find a bug or unexpected behavior, please [file an
+issue](https://github.com/gaballench/StratIntervals.jl/issues).
--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,51 @@
-using StratIntervals
 using Documenter
 
-DocMeta.setdocmeta!(StratIntervals, :DocTestSetup, :(using StratIntervals); recursive=true)
+# copied from the make.jl of PhyloNetworks
+# and modified whenever necessary
+#using Pkg
+#Pkg.add(PackageSpec(name="StratIntervals", rev="main"))
 
-makedocs(;
-    modules=[StratIntervals],
-    authors="Gustavo A. Ballen",
-    sitename="StratIntervals.jl",
-    format=Documenter.HTML(;
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
+using StratIntervals
+DocMeta.setdocmeta!(StratIntervals, :DocTestSetup, :(using StratIntervals); recursive=true)
+#using PhyloPlots # to trigger any precompilation warning outside jldoctests
+
+makedocs(
+    sitename = "StratIntervals.jl",
+    authors = "Gustavo A. Ballen",
+    modules = [StratIntervals], # to list methods from StratIntervals only, not from Base etc.
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true", # easier local build
+        size_threshold = 600 * 2^10, size_threshold_warn = 500 * 2^10), # 600 KiB
+    # exception, so warning-only for :missing_docs. List all others:
+    warnonly = Documenter.except(:autodocs_block, :cross_references, :docs_block,
+        :doctest, :eval_block, :example_block, :footnote, :linkcheck_remotes,
+        :linkcheck, :meta_block, :parse_error, :setup_block),
+    pages = [
         "Home" => "index.md",
+        #"Manual" => [
+        #    "Installation" => "man/installation.md",
+        #    "Network manipulation" => "man/netmanipulation.md",
+        #    "Input Data for SNaQ" => "man/inputdata.md",
+        #    "TICR pipeline" => "man/ticr_howtogetQuartetCFs.md",
+        #    "Network estimation and display" => "man/snaq_plot.md",
+        #    "Network comparison and manipulation" => "man/dist_reroot.md",
+        #    "Candidate Networks" => "man/fixednetworkoptim.md",
+        #    "Extract Expected CFs" => "man/expectedCFs.md",
+        #    "Bootstrap" => "man/bootstrap.md",
+        #    "Multiple Alleles" => "man/multiplealleles.md",
+        #    "Continuous Trait Evolution" => "man/trait_tree.md",
+        #    "Parsimony on networks" => "man/parsimony.md",
+        #    "Discrete Trait Evolution" => "man/fitDiscrete.md",
+        #    "Neighbour Joining" => "man/nj.md",
+        #],
+        #"Library" => [
+        #    "Public" => "lib/public.md",
+        #    "Internals" => "lib/internals.md",
     ],
+
+)
+
+deploydocs(
+    repo = "github.com/gaballench/StratIntervals.jl.git",
+    push_preview = true,
 )
