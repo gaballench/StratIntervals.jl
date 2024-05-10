69e0586e3d2340350282a988a1daa0537af243fc
gaballench <gaballench@gmail.com>
2024-05-10 08:22:05 -0300

    Restructured struct for wrapping up the Turing model

--- src/structs.jl
+++ src/structs.jl
@@ -3,7 +3,7 @@
 # data are being used by the loglik inside the Turing model
 #*_prior are passed on to the parameter priors inside the Turing model 
 struct StratInterval
-    data::Vector{Float64}
+    data::Vector{Real}
     theta1_prior::ContinuousUnivariateDistribution
     theta2_prior::ContinuousUnivariateDistribution
     lambda_prior::ContinuousUnivariateDistribution
--- src/turingmodel.jl
+++ src/turingmodel.jl
@@ -11,24 +11,39 @@
 #
 #data = rand(BetaAdaptive(true_theta1, true_theta2, true_lambda), ndata)
 
-# then declare the Turing model as a hierarchical Bayesian model with
-# priors on theta1, theta2, and lambda
-@model function stratint(tau::Vector{Real})
-    # priors
-    theta1 ~ Normal(0.5,1)
-    theta2 ~ Normal(22,1)
-    lambda ~ Normal(0,1)
-    # reject values outside the support of the parameters
-    if theta1 > minimum(data) || theta2 < maximum(data) || theta1 < 0 || theta2 < 0
-        Turing.@addlogprob! -Inf
-        return nothing
+#interval = StratInterval(data, Exponential(1), Exponential(1), Uniform(0,1))
+
+function sample_stratint(data_priors::StratInterval, iters, epsilon, tau_hmc, prior)
+    # then declare the Turing model as a hierarchical Bayesian model with
+    # priors on theta1, theta2, and lambda
+    @model function stratint(data_priors::StratInterval)
+        # convert to real
+        #data = convert(Vector{Real}, data_priors.data)
+        # priors
+        theta1 ~ data_priors.prior1
+        theta2 ~ data_priors.prior2
+        lambda ~ data_priors.prior3
+        # reject values outside the support of the parameters
+        if theta1 > minimum(data_priors.data) || theta2 < maximum(data_priors.data) || theta1 < 0 || theta2 < 0
+            Turing.@addlogprob! -Inf
+            return nothing
+        end
+        # define the likelihood of the observed data
+        N = length(data_priors.data)
+        for i in 1:N
+            tau[i] ~ BetaAdaptive(theta1, theta2, lambda)
+            #println(tau[i]~ BetaAdaptive(theta1, theta2, lambda))
+        end
     end
-    # define the likelihood of the observed data
-    N = length(data)
-    for i in 1:N
-        tau[i] ~ BetaAdaptive(theta1, theta2, lambda)
-        #println(tau[i]~ BetaAdaptive(theta1, theta2, lambda))
+    # sample from prior? enter block and return chain
+    if prior
+        chain = sample(stratint(data_priors.data, theta1, theta2, lambda), Prior(), HMC(epsilon, tau_hmc), iters)
+        return chain
     end
+    # else, sample from posterior and return chain
+    chain = sample(stratint(data_priors.data, theta1, theta2, lambda), HMC(epsilon, tau_hmc), iters)
+    
+    return chain
 end
 
 #### uncomment for sampling
