d1f82399999c89ee8e7c82812d90c83cb8136790
gaballench <gaballench@gmail.com>
2024-06-17 22:23:49 -0300

    Actually there were some errors inside the turing model but large part of the breaking code was because of a wrong prior on theta2 as well as an incorrect specificaiton of the sampler when deciding to sample from prior or posterior. Closes #5.

--- src/turingmodel.jl
+++ src/turingmodel.jl
@@ -1,21 +1,24 @@
 # wrapper for the the Turing model for a single tratigraphic interval
 function sample_stratinterval(data_priors::StratInterval, iters, ϵ, τ_hmc, prior)
+    # dismantle the StratInterval object
+    data = data_priors.data
+    θ1_prior = data_priors.θ1_prior
+    θ2_prior = data_priors.θ2_prior
+    λ_prior = data_priors.λ_prior    
     # then declare the Turing model as a hierarchical Bayesian model with
     # priors on theta1, theta2, and lambda
-    @model function stratint(data_priors::StratInterval)
-        # convert to real
-        #data = convert(Vector{Real}, data_priors.data)
+    @model function stratint(τ::Vector{Real})
         # priors
-        θ1 ~ data_priors.prior1
-        θ2 ~ data_priors.prior2
-        λ ~ data_priors.prior3
+        θ1 ~ θ1_prior
+        θ2 ~ θ2_prior
+        λ ~ λ_prior
         # reject values outside the support of the parameters
-        if θ1 > minimum(data_priors.data) || θ2 < maximum(data_priors.data) || θ1 < 0 || θ2 < 0
+        if θ1 > minimum(τ) || θ2 < maximum(τ) || θ1 < 0 || θ2 < 0
             Turing.@addlogprob! -Inf
             return nothing
         end
         # define the likelihood of the observed data
-        N = length(data_priors.data)
+        N = length(τ)
         for i in 1:N
             τ[i] ~ BetaAdaptive(θ1, θ2, λ)
             #println(τ[i]~ BetaAdaptive(θ1, θ2, λ))
@@ -23,12 +26,14 @@ function sample_stratinterval(data_priors::StratInterval, iters, ϵ, τ_hmc, pri
     end
     # sample from prior? enter block and return chain
     if prior
-        chain = sample(stratint(data_priors.data, θ1, θ2, λ), Prior(), HMC(ϵ, τ_hmc), iters)
+        chain = sample(stratint(data),
+                       Prior(),
+                       iters)
         return chain
     end
     # else, sample from posterior and return chain
-    chain = sample(stratint(data_priors.data, θ1, θ2, λ), HMC(ϵ, τ_hmc), iters)
+    chain = sample(stratint(data),
+                   HMC(ϵ, τ_hmc),
+                   iters)
     return chain
 end
-
-# sampling from a collection needs to be carried out by defining the likelihood by a conflation of the collection of distributions, but said conflation is univariate and we need to solve the issue of having multiple data vectors one for each distribution. Maybe after we sample, we calculate the conflation as a deterministic node y := conflation(d, x)
