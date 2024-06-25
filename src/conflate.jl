9e9ef237657b1029cac44eddabe03945b66022e1
gaballench <gaballench@gmail.com>
2024-06-24 22:38:59 -0300

    Method for an Array{Float64,2} from posterior predictive analysis of a series of StratInts ready, thus relaxing the need for analytical PDFs which can now use KDE interpolations for calculating empirical pdfs

--- src/conflate.jl
+++ src/conflate.jl
@@ -22,3 +22,15 @@ function conflate(d::Product, x::Float64)
     denominat, err = quadgk(x -> pdf(numerat, x), -Inf, Inf, rtol=1e-8)
     return pdf(numerat, x)/denominat
 end
+
+# conflation method for a matrix of floats, which are coming from a posterior predictive analysis, one column per stratigraphic interval
+function conflate(d::Array{Float64, 2}, x::Float64)
+    # we need to wrap everything related to InterpKDE inside the method Array{Flot64, 2} as the type hierarchy does not allow to easily create a method for it
+    # take a matrix of empirical values to smooth, column-wise, and then apply the KDE and interpolation on each column
+    # then we will have a vector of elements of type InterpKDE which can be called using the pdf method.
+    interpolations = map(i -> InterpKDE(kde(d[:, i])), 1:size(d,2))
+    # numerat not needed beforehand
+    #evaluation of the product pdf is pdf(numerator, [vector of the same value, one per distribution])
+    denominat, err = quadgk(x -> prod(map(dd -> pdf(dd, x), interpolations)), -Inf, Inf, rtol=1e-8)
+    return prod(map(dd -> pdf(dd, x), interpolations))/denominat
+end
