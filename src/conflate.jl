e235df54a1cc421c80895841ae9b832a924b6b54
gaballench <gaballench@gmail.com>
2024-06-24 21:37:27 -0300

    numerator and denominator are already defined, renamed to numerat and denominat in conflate.jl in order to avoid potential issues down the road

--- src/conflate.jl
+++ src/conflate.jl
@@ -14,11 +14,11 @@ end
 
 # conflation needs to be univariate wrt x. It can be vectorised by taking pdf. but this needs to be done carefully in order to check whether Turing will behave correctly in this case
 function conflate(d::Product, x::Float64)
-    numerator = d
+    numerat = d
     #evaluation of the product pdf is pdf(numerator, [vector of the same value, one per distribution])
     # vector of values for product_distribution might be a metaprogramming like paste(rep('x', times =n), sep='*')
     # now integrating with the QuadGK package because Integrals simply did not work out of the box
     # integral, err = quadgk(x -> pdf(Normal(0,1),x), -Inf, Inf, rtol=1e-8)
-    denominator, err = quadgk(x -> pdf(numerator, x), -Inf, Inf, rtol=1e-8)
-    return pdf(numerator, x)/denominator
+    denominat, err = quadgk(x -> pdf(numerat, x), -Inf, Inf, rtol=1e-8)
+    return pdf(numerat, x)/denominat
 end
