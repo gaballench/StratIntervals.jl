196d430ed1e30009193078e035461a701bd509ad
gaballench <gaballench@gmail.com>
2024-09-03 08:52:19 -0300

    Added code for calculating the posterior predictive of a given stratigraphic interval, as well as extended the sampling to receive a Vector{StratInterval} object and return the chains and postpredicts when needed

--- src/turingmodel.jl
+++ src/turingmodel.jl
@@ -1,5 +1,5 @@
 # wrapper for the the Turing model for a single tratigraphic interval
-function sample_stratinterval(data_priors::StratInterval, iters, ϵ, τ_hmc, prior)
+function sample_stratinterval(data_priors::StratInterval, iters, ϵ, τ_hmc, prior, postpredict)
     # dismantle the StratInterval object
     data = data_priors.data
     θ1_prior = data_priors.θ1_prior
@@ -7,7 +7,12 @@ function sample_stratinterval(data_priors::StratInterval, iters, ϵ, τ_hmc, pri
     λ_prior = data_priors.λ_prior    
     # then declare the Turing model as a hierarchical Bayesian model with
     # priors on theta1, theta2, and lambda
-    @model function stratint(τ::Vector{Real})
+    @model function stratint(τ, ::Type{T}=Float64) where {T}
+        # initialise tau if missing for posterior predictive
+        if τ === missing
+            # Initialize `x` if missing
+            τ = Vector{T}(undef, 1)
+        end
         # priors
         θ1 ~ θ1_prior
         θ2 ~ θ2_prior
@@ -26,14 +31,36 @@ function sample_stratinterval(data_priors::StratInterval, iters, ϵ, τ_hmc, pri
     end
     # sample from prior? enter block and return chain
     if prior
-        chain = sample(stratint(data),
+        chain = sample(stratint(convert(Vector{Real}, data)),
                        Prior(),
                        iters)
         return chain
     end
     # else, sample from posterior and return chain
-    chain = sample(stratint(data),
+    chain = sample(stratint(convert(Vector{Real}, data)),
                    HMC(ϵ, τ_hmc),
                    iters)
-    return chain
+    # if posterior predictive, sample from chain and using the model
+    if postpredict
+        pars = DataFrame(chain)[:, [:θ1, :θ2, :λ]]        
+        # generate the posterior predictive from the samples in `chain`
+        # by mapping a rand call to BetaAdaptive with each collection of values 
+        # prepare the results vector
+        postpredict = Vector(undef, iters)        
+        # map over iterations to sample one value from each combination of params
+        postpredict = map(x -> rand(BetaAdaptive(pars.θ1[x], pars.θ2[x], pars.λ[x]), 1)[1], 1:iters)
+        return (chain, postpredict)
+    else
+        return chain
+    end
+end
+
+function sample_stratinterval(stratintervals::Vector{StratInterval}, iters, ϵ, τ_hmc, prior, postpredict)
+    output = Vector(undef, length(stratintervals))
+    counter = 1
+    for i in stratintervals
+        output[counter] = sample_stratinterval(i, iters, ϵ, τ_hmc, prior, postpredict)
+        counter += 1
+    end
+    return output
 end
