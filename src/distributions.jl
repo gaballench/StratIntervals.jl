db885666426dc12ab67d11ce13f58210c0e04380
gaballench <gaballench@gmail.com>
2026-02-02 19:47:48 -0300

    correcting the offset

--- src/distributions.jl
+++ src/distributions.jl
@@ -708,6 +708,5 @@ rand(RefOffExponential(1, 10.0, 1), 10) # return a vector of 10 random numbers
 """
 function Base.rand(rng::AbstractRNG, d::RefOffExponentialSampler)
     #sample = rand(rng, d.distribution(d.θ))
-    #return d.ρ * (sample - d.o) # original
-    return -1
+    return d.ρ * (sample + d.o)
 end
